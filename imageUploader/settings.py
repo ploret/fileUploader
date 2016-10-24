37f4cc302d405ac3ffb6131227ab67c602c09702
lemo_ua <plorett@yandex.ru>
2016-10-24 11:39:33 +0300

    added first action - upload

--- imageUploader/settings.py
+++ imageUploader/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'uploader'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'imageUploader.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'imageUploader/uploaderApp/templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'imageUploader/uploader/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
---
+++ uploader/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'uploader/simple_upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'uploader/simple_upload.html')
